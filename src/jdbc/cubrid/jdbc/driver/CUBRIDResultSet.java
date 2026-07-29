8e0f75e8420807c179436463c2d13d539f2aa166
hwanyseo <hwanyseo@cubrid.com>
2026-07-29 16:31:04 +0900

    wokr1

--- src/jdbc/cubrid/jdbc/driver/CUBRIDResultSet.java
+++ src/jdbc/cubrid/jdbc/driver/CUBRIDResultSet.java
@@ -280,6 +280,10 @@ public class CUBRIDResultSet implements ResultSet {
     }
 
     public void close() throws SQLException {
+        close(true);
+    }
+
+    void close(boolean closeCursor) throws SQLException {
         try {
             synchronized (con) {
                 synchronized (stmt) {
@@ -290,7 +294,9 @@ public class CUBRIDResultSet implements ResultSet {
                         is_closed = true;
 
                         clearCurrentRow();
-                        if (stmt.getResultSetHoldability() == ResultSet.HOLD_CURSORS_OVER_COMMIT) {
+                        if (closeCursor
+                                && stmt.getResultSetHoldability()
+                                        == ResultSet.HOLD_CURSORS_OVER_COMMIT) {
                             u_stmt.closeCursor();
                         }
                         if (complete_on_close) {
